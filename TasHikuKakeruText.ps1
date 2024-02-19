5073af3cd6262817b6cd738dcc1e5cbd7038d220
NanjoMiyako2 <140793758+NanjoMiyako2@users.noreply.github.com>
2024-02-19 17:58:11 +0900

    Add files via upload

---
+++ TasHikuKakeruText.ps1
@@ -0,0 +1,42 @@
+﻿$text1 = ""
+$text2 = ""
+$text3 = ""
+
+for($i = 1; $i -lt 11; $i++){
+
+    for($j=1; $j -lt 11; $j++){
+        $text1 = [string]$i + "たす" + [string]$j + "は"
+        #echo $text1
+        $text2 = [string]($i+$j);
+        #echo $text2
+
+        $text3 = $text1 + "," + $text2
+        echo $text3
+
+        $text1 = [string]$i + "ひく" + [string]$j + "は"
+        #echo $text1
+        $text2 = [string]($i-$j);
+        #echo $text2
+
+        $text3 = $text1 + "," + $text2
+        echo $text3
+
+        $text1 = [string]$i + "かける" + [string]$j + "は"
+        #echo $text1
+        $text2 = [string]($i*$j);
+        #echo $text2
+
+        $text3 = $text1 + "," + $text2
+        echo $text3
+
+    }
+
+}
+
+#テキスト読み上げ用
+Add-Type -AssemblyName System.Speech
+
+#$ps_speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+#$ps_speak.SelectVoice("Microsoft Haruka Desktop")
+#$ps_speak.Speak("1たす8は")
+#$ps_speak.Speak("-5")
---
+++ TasuHikuKakeruRandomYomiagehoge.ps1
@@ -0,0 +1,26 @@
+﻿#テキスト読み上げ用
+Add-Type -AssemblyName System.Speech
+
+$ps_speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$ps_speak.SelectVoice("Microsoft Haruka Desktop")
+
+#音声読み上げ用ファイルを読み込み
+$arr = Get-Content "C:\hogehoge\hon-yomiage-text\tasu-hiku-kakeru-texts1.txt"
+#echo $arr
+
+#読み上げる回数
+$speakCount = 30;
+
+for($i=0; $i -lt $speakCount; $i++){
+
+$idx = Get-Random -Maximum ($arr.Length-1) -Minimum 0
+$txt1 = $arr[$idx].Split(",");
+
+$ps_speak.Speak($txt1[0])
+
+Start-Sleep -s 3
+
+$ps_speak.speak($txt1[1])
+
+
+}
